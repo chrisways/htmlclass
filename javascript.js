bdb9488adc7461a779bc35f09caeef34b4ea303a
chrisways@gmail.com <chrisways@gmail.com>
2018-05-21 14:19:55 +0100

    My commit messege from GIT GUI

---
+++ javascript.js
@@ -0,0 +1,17 @@
+<script>
+var i = 5;
+var u = 7;
+var name = "Lawrence";
+if(i <= 5)
+{
+	if(name.length > 6)
+	{
+		alert("she must be a yoruba girl");
+	}
+	alert('unknown fellow');
+}
+else
+{
+	alert('nobody');
+}
+</script>
